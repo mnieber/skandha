59e19a907f952dc6166f85aca74eecaee9feefac
Maarten Nieber <hallomaarten@yahoo.com>
2020-10-18 20:17:14 +0200

    Add sendMsg and subscribe

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export { input, output, data, isDataMember } from "./lib/data";
 export { handle } from "./lib/handle";
 export { options } from "./internal/options";
 export { listen } from "./lib/listen";
+export { sendMsg, subscribe } from "./lib/msg";
 export { operation, async_opn } from "./lib/operation";
 export { facet, registerFacets, getFacet } from "./lib/facet";
 export { installPolicies } from "./lib/install";
---
+++ lib/msg.ts
@@ -0,0 +1,28 @@
+import { getSignal } from "../internal/events";
+import { getCtr } from "../internal/ctr";
+
+const _type = "facet.Message";
+
+function matchFilename(str: string, rule: string) {
+  var escapeRegex = (str: string) =>
+    str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+  return new RegExp(
+    "^" + rule.split("*").map(escapeRegex).join(".*") + "$"
+  ).test(str);
+}
+
+export function sendMsg(facet, topic, details = {}) {
+  getSignal(getCtr(facet)).send({
+    type: _type,
+    topic,
+    details,
+  });
+}
+
+export function subscribe(facet, messageFilter, callback) {
+  getSignal(getCtr(facet)).add((event) => {
+    if (event.type === _type && matchFilename(event.topic, messageFilter)) {
+      callback(event);
+    }
+  });
+}
