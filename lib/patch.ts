d686f95f1d2eb7eb288c4b9fe64b3d4f678d15f3
Maarten Nieber <hallomaarten@yahoo.com>
2021-06-07 08:11:14 +0200

    Add error message for when facet cannot be deleted

--- lib/patch.ts
+++ lib/patch.ts
@@ -25,7 +25,18 @@ export function patchFacet(facet: any, members: any, options?: any) {
       }
     }
 
-    delete facet[prop];
+    try {
+      delete facet[prop];
+    } catch {
+      console.error(
+        `Could not delete property ${prop} from facet ${facetLogName(facet)}` +
+          ". In case you are using mobx (or skandha-mobx), this can happen " +
+          "when makeObservable (or makeCtrObservable) is called before " +
+          "patching a property. Try calling these functions as late as " +
+          "possible."
+      );
+    }
+
     Object.defineProperty(facet, prop, {
       ...members[prop],
       enumerable: true,
@@ -34,44 +45,48 @@ export function patchFacet(facet: any, members: any, options?: any) {
   }
 }
 
-export const mapDataToFacet = (
-  [toFacetClass, toMember, toCtr]: ClassMemberT,
-  getter: GetterT,
-  transform?: Function
-) => (ctr: any) => {
-  const patch = {
-    [toMember]: {
-      get: () => {
-        const context = {
-          ctr: toCtr ?? ctr,
-          getter,
-          toClassMember: [toFacetClass, toMember],
-          data: getter(ctr),
-        };
-        return transform ? transform(context.data) : context.data;
+export const mapDataToFacet =
+  (
+    [toFacetClass, toMember, toCtr]: ClassMemberT,
+    getter: GetterT,
+    transform?: Function
+  ) =>
+  (ctr: any) => {
+    const patch = {
+      [toMember]: {
+        get: () => {
+          const context = {
+            ctr: toCtr ?? ctr,
+            getter,
+            toClassMember: [toFacetClass, toMember],
+            data: getter(ctr),
+          };
+          return transform ? transform(context.data) : context.data;
+        },
       },
-    },
+    };
+    patchFacet(getf(toFacetClass, toCtr ?? ctr), patch);
   };
-  patchFacet(getf(toFacetClass, toCtr ?? ctr), patch);
-};
 
-export const mapDatasToFacet = (
-  [toFacetClass, toMember, toCtr]: ClassMemberT,
-  getters: Array<GetterT>,
-  transform: Function
-) => (ctr: any) => {
-  const patch = {
-    [toMember]: {
-      get: () => {
-        const context = {
-          ctr: toCtr ?? ctr,
-          getters,
-          toClassMember: [toFacetClass, toMember],
-          datas: getters.map((getter) => getter(ctr)),
-        };
-        return transform(...context.datas);
+export const mapDatasToFacet =
+  (
+    [toFacetClass, toMember, toCtr]: ClassMemberT,
+    getters: Array<GetterT>,
+    transform: Function
+  ) =>
+  (ctr: any) => {
+    const patch = {
+      [toMember]: {
+        get: () => {
+          const context = {
+            ctr: toCtr ?? ctr,
+            getters,
+            toClassMember: [toFacetClass, toMember],
+            datas: getters.map((getter) => getter(ctr)),
+          };
+          return transform(...context.datas);
+        },
       },
-    },
+    };
+    patchFacet(getf(toFacetClass, toCtr ?? ctr), patch);
   };
-  patchFacet(getf(toFacetClass, toCtr ?? ctr), patch);
-};
